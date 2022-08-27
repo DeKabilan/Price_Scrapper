39b7b83aee61c4176ae11fd315b5b0146271418c
DeKabilan <38588004+DeKabilan@users.noreply.github.com>
2022-08-27 16:11:20 +0530

    Finalized

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from ast import Delete
 from telegram import *
 from telegram.ext import *
 import telegram.ext
@@ -10,17 +11,26 @@ load_dotenv()
 token = os.getenv('token')
 updater = telegram.ext.Updater(token, use_context=True)
 dispatcher = updater.dispatcher
+switcheth="switcheth"
+switchbtc="switchbtc"
+
 
 
 
 def start(update, context):
-        update.message.reply_text(
+    #INLINE FOR SWITCH
+     update.message.reply_text(
         """
          Welcome to Price_Checker BOT
 
 Use /help tp know the list of commands
 
         """)
+
+        #INLINE FOR SWITCH
+     buttons = [[InlineKeyboardButton("ETH", callback_data=switcheth)], [InlineKeyboardButton("BTC", callback_data=switchbtc)]]
+     context.bot.send_message(chat_id=update.effective_chat.id, reply_markup= InlineKeyboardMarkup(buttons), text = "What do you want to Switch to?")
+
 
     
 
@@ -37,23 +47,57 @@ def help(update, context):
         """
     )
 
-def btc(update, context):
-    update.message.reply_text(namebtc)
-    update.message.reply_text(pricebtc)
+def btc(update: Update, context: CallbackContext):
+     buttons1 = [[KeyboardButton(namebtc)], [KeyboardButton(pricebtc)]]
+     context.bot.send_message(chat_id=update.effective_chat.id,text="BTC PRICE",
+     reply_markup=ReplyKeyboardMarkup(buttons1))
 
-def eth(update, context):
-    update.message.reply_text(nameeth)
-    update.message.reply_text(priceeth)
+    #  #INLINE FOR SWITCH
+     buttons = [[InlineKeyboardButton("ETH", callback_data=switcheth)], [InlineKeyboardButton("BTC", callback_data=switchbtc)]]
+     context.bot.send_message(chat_id=update.effective_chat.id, reply_markup= InlineKeyboardMarkup(buttons), text = "What do you want to Switch to?")
+
+def eth(update: Update, context: CallbackContext):
+     buttons1 = [[KeyboardButton(nameeth)], [KeyboardButton(priceeth)]]
+     context.bot.send_message(chat_id=update.effective_chat.id,text="ETH PRICE",
+     reply_markup=ReplyKeyboardMarkup(buttons1))
+
+    #  #INLINE FOR SWITCH
+     buttons = [[InlineKeyboardButton("ETH", callback_data=switcheth)], [InlineKeyboardButton("BTC", callback_data=switchbtc)]]
+     context.bot.send_message(chat_id=update.effective_chat.id, reply_markup= InlineKeyboardMarkup(buttons), text = "What do you want to Switch to?")
+
+        
 
 def dev(update, context):
     update.message.reply_text("https://github.com/DeKabilan")
 
+#QUERY_Handler
+def queryHandler(update: Update, context = CallbackContext):
+    query = update.callback_query.data
+    update.callback_query.answer()
+    global switcheth, switchbtc
 
+    if switcheth in query:
+        buttons1 = [[KeyboardButton(nameeth)], [KeyboardButton(priceeth)]]
+        context.bot.send_message(chat_id=update.effective_chat.id,text="ETH PRICE",
+     reply_markup=ReplyKeyboardMarkup(buttons1))
+        
+        
+    if switchbtc in query:
+        buttons1 = [[KeyboardButton(namebtc)], [KeyboardButton(pricebtc)]]
+        context.bot.send_message(chat_id=update.effective_chat.id,text="BTC PRICE",
+     reply_markup=ReplyKeyboardMarkup(buttons1))
+        
+        
+
+
+
+dispatcher.add_handler(CallbackQueryHandler(queryHandler))
 dispatcher.add_handler(telegram.ext.CommandHandler("start", start))
 dispatcher.add_handler(telegram.ext.CommandHandler("help", help))
+dispatcher.add_handler(telegram.ext.CommandHandler("dev", dev))
 dispatcher.add_handler(telegram.ext.CommandHandler("btc", btc))
 dispatcher.add_handler(telegram.ext.CommandHandler("eth", eth))
-dispatcher.add_handler(telegram.ext.CommandHandler("dev", dev))
+
 
 updater.start_polling()
 updater.idle()
